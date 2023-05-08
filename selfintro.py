83de5c38be050225f22b6d1ef30c1674e74b9353
vishnummVmm <132332560+vishnummVmm@users.noreply.github.com>
2023-05-08 12:00:04 +0530

    python

---
+++ selfintro.py
@@ -0,0 +1,7 @@
+b="my self:"
+name="vishnu"
+age=19
+place="chelakkara"
+college="GPTC CHELAKKARA"
+course="software engineer"
+print("SELF INTRODUCTION\n-------------------\n-my name is",name,"\n-I am",age, "years old","\n-I am from",place,"\n-I am studying at",college,"\n-I am a",course,"student"  )
