f2cc64ce065b03da4b719914c12dc581a4ae4ec4
vishnummVmm <132332560+vishnummVmm@users.noreply.github.com>
2023-05-08 15:02:28 +0530

    Create largestno.py

---
+++ largestno.py
@@ -0,0 +1,24 @@
+''' largest amoung three number'''
+
+
+x = int(input("Enter First Number:"))
+y = int(input("Enter Second Number:"))
+z = int(input("Enter third Number:"))
+
+        
+    
+if (x>=y):
+   
+   if(x>=z):
+       print(x)
+   else: 
+        print(z)
+elif (y>=x):
+   
+   if(y>=z):
+       print(y)
+   else: 
+        print(z)
+
+
+
