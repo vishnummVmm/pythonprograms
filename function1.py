912f2dfdda89946a6a9a8b70338a6adf3f9de016
vishnummVmm <132332560+vishnummVmm@users.noreply.github.com>
2023-05-09 14:28:55 +0530

    Create function1.py

---
+++ function1.py
@@ -0,0 +1,10 @@
+''' sum of two numbers using function'''
+
+
+a=int(input( "enter first number "))
+b=int(input("enter second number"))
+def add(m,n):
+    sum1=n+m
+    return(sum1)
+x=add(a,b)
+print(x)
