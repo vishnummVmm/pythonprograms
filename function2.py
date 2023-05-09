9f8541793a5244614702be5a3772906552823b6d
vishnummVmm <132332560+vishnummVmm@users.noreply.github.com>
2023-05-09 14:48:44 +0530

    Create function2.py

---
+++ function2.py
@@ -0,0 +1,25 @@
+''' sum of two numbers using function'''
+
+
+a=float(input( "enter first number "))
+b=float(input("enter second number"))
+def add(m,n):
+    sum1=m+n
+    return(sum1)
+x=add(a,b)
+print("sum is",x)
+def sub(m,n):
+    sub=m-n
+    return(sub)
+y=sub(a,b)
+print("subtracted value is",y)
+def mul(m,n):
+    mul=m*n
+    return(mul)
+z=mul(a,b)
+print("multiplied value is",z)
+def div(m,n):
+    div=m/n
+    return(div)
+k=div(a,b)
+print("divided value is",k)
